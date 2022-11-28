82492bfb7df7c98bb92b15598833fa1a0b887f11
riyad5541 <abdullahalriyad5541@gmail.com>
2022-11-28 11:37:43 +0600

    instell stripe and setup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { query } = require('express');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 
@@ -93,6 +94,23 @@ async function run(){
 
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
+        })
+
+        app.post('/create-payment-intent', async (req, res) =>{
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types" : [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            })
         })
 
         app.get('/jwt', async(req, res) =>{
